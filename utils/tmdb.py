6d6211007ba9f2dc75a588a98a1466b0990dd669
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-08-17 19:00:38 +0530

    .

--- utils/tmdb.py
+++ utils/tmdb.py
@@ -47,7 +47,7 @@ class TMDB(object):
             query = query.replace(yr, "").strip()
             post_data = {"query": query, "year": yr}
         res = self._request_connection(path, post_data)
-        return self.unify(res.json())
+        return self.unify(self,res.json())
     def search_tv(self, query:str)->dict:
         path = "/search/tv"
         query = clean_name(query)
@@ -57,7 +57,7 @@ class TMDB(object):
             query = query.replace(yr, "").strip()
             post_data = {"query": query, "year": yr}
         res = self._request_connection(path, post_data)
-        return self.unify(res.json(),tv=True)
+        return self.unify(self,res.json(),tv=True)
     def to_imdb(self, tmdb_id,tv=False)->str|int:
         path = f"/movie/{tmdb_id}/external_ids"
         if tv:
