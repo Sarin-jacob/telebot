77f0589ef8c677a922f4942ba5636983eebac569
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-08-17 19:46:55 +0530

    .

--- telebot.py
+++ telebot.py
@@ -235,15 +235,18 @@ with TelegramClient(getSession(), api_id, api_hash).start() as client:
             await start_bot_client()
             entity = await bot_client.get_entity(channel_id)
             res=tmdb.search_tv(query) if tv else tmdb.search_movie(query)
-            if len(set(res["imdb_id"])) > 3:
-            # Create button markup
-                filtered_data = [(title, year, imdb_id) for title, year, imdb_id in zip(res["title"], res["year"], res["imdb_id"]) if imdb_id is not None]
-                buttons = [[Button.url(f"{title} ({year})", f"https://www.imdb.com/title/{imdb_id}")]
-                       for title, year, imdb_id in filtered_data]
-            else:
-                #get index of non empty imdb_id
-                idx=[i for i, e in enumerate(res["imdb_id"]) if e != '']
-                buttons = [Button.url(f"{res['title'][i]} ({res['year'][i]})", f"https://www.imdb.com/title/{res['imdb_id'][i]}") for i in idx]
+
+            filtered_data = [(title, year, imdb_id) for title, year, imdb_id in zip(res["title"], res["year"], res["imdb_id"]) if imdb_id is not None]
+            if len(filtered_data) == 0:
+                await bot_client.send_message(entity, "No results found",silent=True)
+                return
+            elif len(filtered_data) ==1:
+                await bot_client.send_message(entity, "Unique Result Found",silent=True)
+                return
+            elif len(filtered_data) > 10:
+                filtered_data = filtered_data[:10]
+            buttons = [[Button.url(f"{title} ({year})", f"https://www.imdb.com/title/{imdb_id}")]
+                    for title, year, imdb_id in filtered_data]
             await bot_client.send_message(entity, "Search Results:", buttons=buttons,silent=True)
 
 
