51b184adbd89f24de13caf47eca8634c0a37c1c6
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-08-19 01:34:39 +0530

    test

--- utils/imdbs.py
+++ utils/imdbs.py
@@ -1,6 +1,7 @@
 import imdb
 ia=imdb.Cinemagoer()
-# test=ia.search_movie('winter soldier')
+test=ia.search_movie('winter soldier')
+print(test[0].keys())
 # print(test[0:6])
 def search_files(query):
     mov=[]
@@ -8,7 +9,7 @@ def search_files(query):
     moser=ia.search_movie(query)
     for i in moser:
         try:
-            temp=(i["title"],i["year"],i["kind"],i.movieID)
+            temp=(i["kind"],i["title"],i["year"],i.movieID)
         except:
             continue
         if temp[2] in ['movie', 'tv movie', 'short']:
@@ -16,9 +17,9 @@ def search_files(query):
         elif temp[2] in ['tv series', 'tv mini series', 'tv special', 'tv short']:
             ser.append(temp)
     return mov, ser
-a,b=search_files('winter soldier')
-for i in a:
-    print(i)
-print()
-for i in b:
-    print(i)
+# a,b=search_files('winter soldier')
+# for i in a:
+#     print(i)
+# print()
+# for i in b:
+#     print(i)
