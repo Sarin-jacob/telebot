b97e24889410222ec1e14373ee8069714ed6fbd8
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-08-19 02:11:46 +0530

    .

--- telebot.py
+++ telebot.py
@@ -198,8 +198,8 @@ with TelegramClient(getSession(), api_id, api_hash).start() as client:
         query=event.pattern_match.group(1).strip()
         cn=clean_name(query)
         mv, sr = search_files(cn)
-        print(f"{cn = } {mv = } {sr = } type = {type(mv)}")
-        filtered_data = mv.extend(sr)
+        print(f"{cn = } {mv = } {sr = } type = {type(mv + sr)}")
+        filtered_data = mv + sr
         snp=re.compile(r"[sS]\d{2}([eE]\d{2})?")
         sinfo=snp.search(query)
         sinfo = sinfo.group() if sinfo else None
