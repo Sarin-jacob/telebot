28ad0a6d5e774121e38f19019e8efec81d0b45ab
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-08-19 01:08:11 +0530

    .

--- utils/imdbs.py
+++ utils/imdbs.py
@@ -6,6 +6,8 @@ def search_files(query):
     dlist=[]
     moser=ia.search_movie(query)
     for i in moser:
+        print(i.items())
         dlist.append((i["title"],i.movieID))
+
     return dlist
 print(search_files('winter soldier'))
