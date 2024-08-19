61680c586080e829e20325d0dd6b2f5adb7afa6a
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-08-19 23:12:00 +0530

    .

--- telebot.py
+++ telebot.py
@@ -460,7 +460,8 @@ with TelegramClient(getSession(), api_id, api_hash).start() as client:
                     try:
                         await msgo("tetsing latest")
                         txtm='⭕️ Latest HD Releases. \n〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n'
-                        msg=await client.get_messages(-1002171035047,reversed=True,limit=5)
+                        msg=await client.get_messages(-1002171035047,ids=2)
+                        await msgo(f"got message:{msg[0].id},{msg=}")
                         firstmg=msg[0]
                         await firstmg.edit(txtm)
                         await client.edit_message(-1002171035047,msg[2].id," wojvv")
