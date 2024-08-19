bcf44715e9ca6c843d78ef203d0bc094ab48b4a1
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-08-19 22:22:29 +0530

    test.

--- telebot.py
+++ telebot.py
@@ -433,6 +433,9 @@ with TelegramClient(getSession(), api_id, api_hash).start() as client:
                     # except Exception as e:
                         # output+=str(e)
                 elif "mov" == command[:3]:
+                    if command[3]==1:
+                        msgo("I worky\n now do rest f the code")
+                        return
                     output='Processing...'
                     prt=valve[4:]
                     for i in prt.split(','):
