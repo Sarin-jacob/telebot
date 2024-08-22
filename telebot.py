c22890810d79572b658250a8ff9a9e3f4419deb3
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-08-22 22:23:57 +0530

    .

--- telebot.py
+++ telebot.py
@@ -279,8 +279,7 @@ with TelegramClient(getSession(), api_id, api_hash).start() as client:
                 # message=message.replace(nm,f"[{nm}](https://www.imdb.com/title/{imdb})")
                 link_text ='IMDb Link'
                 link=f"** [{link_text}](https://www.imdb.com/title/tt{imdb})**"
-                width = max(len(line) for line in message.split('\n'))
-                message=f"{message}\n\n{' '*int((width +len(link_text)+2)/2)}{link}"
+                message=f"{message}\n\n{link}"
             search_url = f"tg://resolve?domain={searchbot}&text={name}"
             if strt==1:
                 search_url = f"tg://resolve?domain={searchbot}&start=search_{name.replace('%20','_')}"
