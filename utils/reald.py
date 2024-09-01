598b9795983742f63cc8037d3869ac1905779cc6
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-09-01 18:57:43 +0530

    .

--- utils/reald.py
+++ utils/reald.py
@@ -7,7 +7,8 @@ def shot_bird(link,dir=None):
     dir=dir if dir else "."
     ba=rd.unrestrict.link(link=link).json()
     print("Downloading: {ba['filename']} \n Size: {ba['size']}\nlink: {ba['download']}")
-    bt=get(ba["download"],stream=True)
+    ln = ba["download"].replace("http://", "https://")
+    bt=get(ln,stream=True)
     with open(f"/{ba['filename']}","wb") as f:
         f.write(bt.content)
     return f"{dir}/{ba['filename']}"
