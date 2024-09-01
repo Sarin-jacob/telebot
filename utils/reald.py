caf76c3184a4fbf17a703f6023f9c7ea28568b2f
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-09-01 16:38:17 +0530

    .

--- utils/reald.py
+++ utils/reald.py
@@ -1,8 +1,9 @@
+import os
 try:
     from rdapi import RD
 except ImportError:
     print("run `pip install rdapi`")
     exit(1)
-
+print(os.environ)
 rd = RD()
 print(rd.unrestrict.link(link="https://1fichier.com/?ophpf2bhyp7cqgu0zmik").json())
