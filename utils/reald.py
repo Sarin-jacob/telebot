e219dcaaa984f4212ff8821e37c4d609a3e88019
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-09-01 19:58:50 +0530

    .

--- utils/reald.py
+++ utils/reald.py
@@ -1,14 +1,64 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+# from rdapi import RD
+# from requests import get
+# rd = RD()
+# def shot_bird(link,dir=None):
+#     dir=dir if dir else "."
+#     ba=rd.unrestrict.link(link=link).json()
+#     print("Downloading: {ba['filename']} \n Size: {ba['size']}\nlink: {ba['download']}")
+#     ln = ba["download"].replace("http://", "https://")
+#     bt=get(ln,stream=True)
+#     with open(f"{dir}/{ba['filename']}","wb") as f:
+#         f.write(bt.content)
+#     return f"{dir}/{ba['filename']}"
+
+###########################
 from dotenv import load_dotenv
 load_dotenv()
 from rdapi import RD
 from requests import get
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
+
 rd = RD()
-def shot_bird(link,dir=None):
-    dir=dir if dir else "."
-    ba=rd.unrestrict.link(link=link).json()
-    print("Downloading: {ba['filename']} \n Size: {ba['size']}\nlink: {ba['download']}")
+
+def download_chunk(url, start, end, dir, filename):
+    headers = {'Range': f'bytes={start}-{end}'}
+    response = get(url, headers=headers, stream=True)
+    chunk_path = f"{dir}/{filename}.part{start}-{end}"
+    with open(chunk_path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+    return chunk_path
+
+def merge_chunks(filename, dir, num_chunks):
+    with open(f"{dir}/{filename}", "wb") as outfile:
+        for i in range(num_chunks):
+            chunk_path = f"{dir}/{filename}.part{i}"
+            with open(chunk_path, "rb") as infile:
+                outfile.write(infile.read())
+            os.remove(chunk_path)
+
+def shot_bird(link, dir=None, num_chunks=4):
+    dir = dir if dir else "."
+    ba = rd.unrestrict.link(link=link).json()
+    print(f"Downloading: {ba['filename']} \n Size: {ba['size']}\nlink: {ba['download']}")
     ln = ba["download"].replace("http://", "https://")
-    bt=get(ln,stream=True)
-    with open(f"{dir}/{ba['filename']}","wb") as f:
-        f.write(bt.content)
+    
+    file_size = int(ba['size'])
+    chunk_size = file_size // num_chunks
+    
+    with ThreadPoolExecutor(max_workers=num_chunks) as executor:
+        futures = []
+        for i in range(num_chunks):
+            start = i * chunk_size
+            end = start + chunk_size - 1 if i < num_chunks - 1 else file_size - 1
+            futures.append(executor.submit(download_chunk, ln, start, end, dir, ba['filename']))
+        
+        for future in as_completed(futures):
+            future.result()
+    
+    merge_chunks(ba['filename'], dir, num_chunks)
     return f"{dir}/{ba['filename']}"
