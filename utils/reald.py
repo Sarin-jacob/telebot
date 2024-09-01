ca5d9c82b6591086c97316b19772b0a6848cac27
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-09-01 18:39:33 +0530

    .

--- utils/reald.py
+++ utils/reald.py
@@ -8,6 +8,6 @@ def shot_bird(link,dir=None):
     ba=rd.unrestrict.link(link=link).json()
     print("Downloading: {ba['filename']} \n Size: {ba['size']}\nlink: {ba['download']}")
     bt=get({ba["download"]},stream=True)
-    with open(f"/{ba["filename"]}","wb") as f:
+    with open(f"/{ba['filename']}","wb") as f:
         f.write(bt.content)
     return f"{dir}/{ba["filename"]}"
