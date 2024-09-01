0cd6a0c41af1e6d86a5a80a166083b2e7caa3f33
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-09-01 16:44:52 +0530

    .

--- utils/reald.py
+++ utils/reald.py
@@ -7,4 +7,5 @@ except ImportError:
     print("run `pip install rdapi`")
     exit(1)
 rd = RD()
+print(rd.unrestrict.link(link="https://www.youtube.com/watch?v=xvFZjo5PgG0").json())
 print(rd.unrestrict.link(link="https://1fichier.com/?ophpf2bhyp7cqgu0zmik").json())
