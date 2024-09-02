66f1b17ba70c3e38c9d3c884e5acf65202443856
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-09-02 20:00:50 +0530

    .

--- utils/funcs.py
+++ utils/funcs.py
@@ -15,34 +15,29 @@ def walker(directory):
     return files_list
 
 def extract_file(file_path):
-    dots=file_path.split('.')
-    extracted_file_path='.'.join(dots[:-1])
-    extt = dots[-1].lower()
+    extt = file_path.split('.')[-1].lower()
     folder_path = path.dirname(file_path)
-    
     if extt in ['zip', 'rar', 'tar']:
         if extt == 'zip':
             with zipfile.ZipFile(file_path, 'r') as zip_ref:
                 zip_ref.extractall(folder_path)
+                extracted_files = zip_ref.namelist()
         elif extt == 'rar':
             with rarfile.RarFile(file_path, 'r') as rar_ref:
                 rar_ref.extractall(folder_path)
+                extracted_files = rar_ref.namelist()
         elif extt == 'tar':
             with tarfile.open(file_path, 'r') as tar_ref:
                 tar_ref.extractall(folder_path)
-        
+                extracted_files = [member.name for member in tar_ref.getmembers()]
         # Remove the original archive file
         remove(file_path)
-        if path.isfile(extracted_file_path):
-            return extracted_file_path
-        elif path.isdir(extracted_file_path):
-            a=walker(extracted_file_path)
-            for i,j in enumerate(a):
-                if j.split('.')[-1] in ['url','txt']:
-                    remove(j)
-                    a.pop(i)
-            return a
-        return None
+        extracted_file_path = [path.join(folder_path, f) for f in extracted_files]
+        for i,j in enumerate(extracted_file_path):
+            if j.split('.')[-1] in ['url','txt','idx','sub']:
+                remove(j)
+                extracted_file_path.pop(i)
+        return extracted_file_path
     else:
         return None
 
