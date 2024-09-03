232fc7109f824e05400a349e65d62fe22e48e118
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-09-03 21:54:27 +0530

    .

--- utils/reald.py
+++ utils/reald.py
@@ -14,6 +14,8 @@
 #     return f"{dir}/{ba['filename']}"
 
 ###########################
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from .info import PARALLEL_DOWNLOADS
 print(PARALLEL_DOWNLOADS)
 from dotenv import load_dotenv
