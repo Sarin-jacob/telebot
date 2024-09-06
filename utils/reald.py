d6ca05acb1ed86d3034a56ed4e9d30e450a2b49a
Sarin C Jacob <53591235+Sarin-jacob@users.noreply.github.com>
2024-09-06 17:00:25 +0530

    .

--- utils/reald.py
+++ utils/reald.py
@@ -76,7 +76,7 @@ def shot_bird(link, dir=None, num_chunks=PARALLEL_DOWNLOADS):
     print(f"Downloading: {ba['filename']} \n Size: {ba['filesize']}\nlink: {ba['download']}")
     # ln = ba["download"].replace("http://", "https://")
     ln=ba['download']
-    filename = get_unique_filename(ba['filename'], ba['filesize'])
+    filename = get_unique_filename(ba['filename'], ba['filesize']).replace("+", " ")
     file_size = int(ba['filesize'])
     if file_size == 0:
         print(f"File size is zero for {ba['filename']}. Downloading as a single chunk.")
